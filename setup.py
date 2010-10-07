ab2f63a99077d207ee420e449b861720d7ab2c8e
Tristan Waddington <tristanw@darkhorse.com>
2010-10-07 11:27:38 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-nova',
+    version='0.0.1',
+    description='Simple newsletters for Django sites.',
+    author='Lennon Day-Reynolds',
+    url='http://bitbucket.org/rcoder/django-nova',
+    packages=find_packages(exclude=['testproject']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    package_data={'': []},
+    zip_safe=False,
+)
