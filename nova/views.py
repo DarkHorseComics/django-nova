68870b29277050f399e21107c8ec72ea7fc473b4
Tristan Waddington <tristanw@darkhorse.com>
2010-10-07 17:17:48 -0700

    Gracefully handle duplicate subscription requests. Added error.html template.

--- nova/views.py
+++ nova/views.py
@@ -1,6 +1,8 @@
 """
 Newsletter registration views
 """
+from datetime import datetime, timedelta
+
 from django.conf import settings
 from django.template import RequestContext, Context, loader 
 from django.shortcuts import render_to_response, get_object_or_404
@@ -18,7 +20,8 @@ def _send_message(to_addr, subject_template, body_template, context_vars):
     for the subject line and body.
     """
     context = Context(context_vars)
-    subject = loader.get_template(subject_template).render(context)
+    # Strip newlines from subject
+    subject = loader.get_template(subject_template).render(context).strip()
     body = loader.get_template(body_template).render(context)
     send_mail(subject, body, settings.DEFAULT_MAIL_FROM, (to_addr,))
 
@@ -28,18 +31,46 @@ def subscribe(request):
     """
     if request.method == 'POST':
         email = request.POST['email']
-        subscription = Subscription.objects.create_with_random_token(email)
-        request.session['subscription'] = subscription
-        _send_message(
-            email, 
-            'nova/email/subscribe_subject.txt',
-            'nova/email/subscribe_body.txt',
-            {
-                'subscription': subscription, 
-                'site': RequestSite(request)
-            }
-        )
-        response = redirect_to(request, reverse(acknowledge))
+        send_email = False
+
+        # Check to see if this email is already subscribed and confirmed
+        try:
+            # Subscription exists
+            subscription = Subscription.objects.get(email=email)
+            if subscription.confirmed:
+                # Subscription already confirmed
+                message = "Thank you! We've previously confirmed your subscription."
+                response = render_to_response('nova/error.html',
+                                              {'error': message},
+                                              RequestContext(request))
+            elif (datetime.now() - subscription.created_at) < timedelta(minutes=15):
+                # Subscription not confirmed and rate limited
+                message = "You've previously submitted a subscription request. Please check %s for your confirmation email or try again in a few minutes." % email
+                response = render_to_response('nova/error.html',
+                                              {'error': message},
+                                              RequestContext(request))
+            else:
+                # Create new subscription
+                subscription.delete()
+                subscription = Subscription.objects.create_with_random_token(email)
+                send_email = True
+        except Subscription.DoesNotExist:
+            # New subscription
+            subscription = Subscription.objects.create_with_random_token(email)
+            send_email = True
+
+        if send_email:
+            request.session['subscription'] = subscription
+            _send_message(
+                email, 
+                'nova/email/subscribe_subject.txt',
+                'nova/email/subscribe_body.txt',
+                {
+                    'subscription': subscription, 
+                    'site': RequestSite(request)
+                }
+            )
+            response = redirect_to(request, reverse(acknowledge))
     else:
         response = render_to_response('nova/subscribe.html')
 
