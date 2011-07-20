801678f2a3296b420572ab3662a87125efcdc1d7
Tristan Waddington <tristanw@darkhorse.com>
2011-07-19 17:02:51 -0700

    Update some copy in the setup.py file. Bump version from 0.0.1 to 0.1.0.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-nova',
-    version='0.0.1',
+    version='0.1.0',
     description='Simple newsletters for Django sites.',
-    author='Lennon Day-Reynolds',
-    url='http://bitbucket.org/rcoder/django-nova',
+    author='Dark Horse Comics'
+    url='http://bitbucket.org/darkhorse/django-nova',
     package_dir={'nova': 'nova'},
     include_package_data=True,
     packages=find_packages(exclude=['testproject']),
